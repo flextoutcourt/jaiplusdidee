290ee7d298c85d013a04a497a95bb7aebde3ad2d
Flex <flextoutcourt@gmail.com>
2024-06-16 16:04:53 +0200

    authentication endpoints

--- src/api/v1/agents/index.js
+++ src/api/v1/agents/index.js
@@ -1,10 +1,22 @@
 import express from "express";
-import prisma from "@/modules/core/lib/prisma"
+// import prisma from "@/modules/core/lib/prisma"
+
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient();
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const agents = await prisma.agent.findMany();
+    const agents = await prisma.agent.findMany({
+        include:{
+            Service: {
+                include: {
+                    entity: true
+                }
+            },
+            Civilian: true
+        }
+    })
     res.json({data: agents});
 });
 
---
+++ src/api/v1/auth/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+// import prisma from "@/modules/core/lib/prisma"
+
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient();
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.json({data: req});
+});
+
+router.post('/', async (req, res, next) => {
+    const {matricule, password} = req.body;
+    const agent = await prisma.agent.findFirstOrThrow({
+        where: {
+            matricule
+        }
+    });
+    if(agent.password === password){
+        res.status(201).json({data: agent});
+    }else{
+        res.status(400).json({message: 'Bad credentials !'})
+    }
+})
+
+export default router;
