888995fd0d42a9bb88809ce98635e79c0525ad7f
Flex <flextoutcourt@gmail.com>
2024-06-19 03:45:38 +0200

    update background-color

--- src/modules/core/components/layouts/Content.tsx
+++ src/modules/core/components/layouts/Content.tsx
@@ -15,13 +15,13 @@ export default function Content({children}: PropsWithChildren) {
             <div className="px-16">
                 <div className="grid grid-cols-9 gap-4">
                     <div className={!user ? "col-span-9" : "col-span-7"}>
-                        <div className="bg-white h-screen rounded-md p-4 max-h-[calc(100vh-19.75rem)] overflow-auto shadow-lg">
+                        <div className="bg-gray-200 h-screen rounded-md p-4 max-h-[calc(100vh-19.75rem)] overflow-auto shadow-lg">
                             {children}
                         </div>
                     </div>
                     {user && 
                         <div className="col-span-2">
-                            <div className="bg-white h-screen rounded-md max-h-[calc(100vh-19.75rem)] overflow-auto p-4">
+                            <div className="bg-white shadow-lg h-screen rounded-md max-h-[calc(100vh-19.75rem)] overflow-auto p-4">
                                 <ShiftProvider>
                                     <ChannelProvider>
                                         <ShiftList />
