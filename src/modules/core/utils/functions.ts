9f0f429e2df80a0475289a40d6abc9fddf3f24b0
Flex <flextoutcourt@gmail.com>
2024-06-19 03:46:16 +0200

    groupByFunction

--- src/modules/core/utils/functions.ts
+++ src/modules/core/utils/functions.ts
@@ -3,3 +3,17 @@ export async function fetcher(url: string){
     const response = await promise.json();
     return response.data;
 }
+
+export function groupByKey<T>(array: T[], key: keyof T): Record<string, T[]>{
+    return array.reduce((accumulator, currentValue) => {
+        const keyValue = currentValue[key];
+        const groupKey = keyValue as unknown as string;
+
+        if(!accumulator[groupKey]){
+            accumulator[groupKey] = [];
+        }
+
+        accumulator[groupKey].push(currentValue);
+        return accumulator;
+    }, {} as Record<string, T[]>);
+}
